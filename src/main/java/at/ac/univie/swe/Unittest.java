18b6e4d9411443dd411c17c319e4905736139d85
Imamovic Huso <a01048686@unet.univie.ac.at>
2017-12-08 19:57:49 +0100

    Update Unit test

--- src/main/java/at/ac/univie/swe/Unittest.java
+++ src/main/java/at/ac/univie/swe/Unittest.java
@@ -10,22 +10,27 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author imamovichuso
  *
  */
 public class Unittest {
-	static GameRepository gameRepository;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	public void mytest() throws Exception {
 		Game demogame = new Game("Demo game",5, 3, 5, 100);
-			assertEquals("Demo game",demogame.getName());
-		Player x = new Player("John", "Smith", "john.smith@example.com");
-		assertEquals("John",x.getFirstName());
+		
+		assertEquals("Demo game",demogame.getName());
+		
+	Player x = new Player("John", "Smith", "john.smith@example.com");
+		
+	assertEquals("John",x.getFirstName());
+		
+		
 	}
 
 	
